ba4a37c85dd39ab0dc639c25bc37d1c87e397c89
anton-bregolas <106439112+anton-bregolas@users.noreply.github.com>
2023-08-02 03:35:06 +0300

    SF Module 10 Task 3: Echo chat completed, commands & geolocation added

--- module10_task3/module10_task3.js
+++ module10_task3/module10_task3.js
@@ -14,7 +14,7 @@ function wsConnect() {
 
   ws.onopen = function(evt) {
     console.log("Connected to server!");
-    showUserMsg("Connected to server!", "server");
+    showUserMsg("Connected to server!", "log");
     connected = 1;
   };
 
@@ -28,7 +28,7 @@ function wsConnect() {
     if (evt.data !== `[object GeolocationPosition]`)
     {
       console.log(`Received message: '${evt.data}'`);
-      showUserMsg(evt.data, "server");
+      showUserMsg(evt.data, "echo");
     }
   };
 
@@ -41,93 +41,116 @@ function wsConnect() {
 function initButtons() {
 
   sendBtn.addEventListener('click', () => {
-    let msg = inputBox.value;
 
-    if (msg && connected == 1) {
+    sendMessage();
 
-      switch(msg) {
-
-        case "/disconnect":
-          console.log('Disconnecting from server...');
-          ws.close();
-          ws = null;
-          break;
-
-        case "/connect":
-          console.log('Active connection detected!');
-          showUserMsg("Already connected!", "error");
-          break;
-
-        case "/help":
-          console.log('Displaying help message...');
-          showUserMsg(`
-          {any}: get echo server response
-          /disconnect: close connection
-          /connect: reconnect to server
-          /help: view list of commands
-          `);
-          break;
-
-        default:
-          console.log(`Sending '${msg}' to server...`);
-          showUserMsg(msg, "user");
-          ws.send(msg);
-      }
-
-    } else if (msg && connected == 0) {
-
-      switch(msg) {
-
-        case "/connect":
-          console.log('Connecting to server...');
-          wsConnect();
-          break;
-        
-        case "/help":
-          console.log('Displaying help message...');
-          showUserMsg(`
-          /connect: reconnect to server
-          /help: view list of commands
-          `);
-          break;
-
-        default:
-          console.log('No connection detected!');
-          showUserMsg("Not connected to the server!", "error");
-      }
-    }
-    inputBox.value = "";
   });
   
   geoBtn.addEventListener('click', () => {
 
-    const geoError = () => {
-      console.log('Error getting geolocation data!');
-      showUserMsg("Unable to get geolocation!", "error");
+    sendGeo();
+
+  });
+}
+
+function sendMessage() {
+
+  let msg = inputBox.value;
+
+  if (msg && connected == 1) {
+
+    switch(msg) {
+
+      case "/disconnect":
+        console.log('Disconnecting from server...');
+        ws.close();
+        ws = null;
+        break;
+
+      case "/connect":
+        console.log('Active connection detected!');
+        showUserMsg("Already connected!", "error");
+        break;
+
+      case "/help":
+        console.log('Displaying help message...');
+        showUserMsg(`
+        {any}: get echo server response
+        /disconnect: close connection
+        /connect: reconnect to server
+        /help: view list of commands
+        `, "user");
+        break;
+
+      default:
+        console.log(`Sending '${msg}' to server...`);
+        showUserMsg(msg, "user");
+        ws.send(msg);
     }
 
-    const geoSuccess = (position) => {
-      console.log('Getting coordinates:', position);
-      ws.send(position);
-      const latitude  = position.coords.latitude;
-      const longitude = position.coords.longitude;
+  } else if (msg && connected == 0) {
+
+    switch(msg) {
+
+      case "/connect":
+        console.log('Connecting to server...');
+        wsConnect();
+        break;
       
-      showUserMsg(`
-      My latitude is ${latitude}° and
-      my longitude is ${longitude}°
-      `, "user");
+      case "/help":
+        console.log('Displaying help message...');
+        showUserMsg(`
+        /connect: reconnect to server
+        /help: view list of commands
+        `, "user");
+        break;
 
-      showUserMsg(`https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`, `user`, true);
+      default:
+        console.log('No connection detected!');
+        showUserMsg("Not connected to the server!", "error");
     }
+  }
 
-    if (!navigator.geolocation) {
-      console.log('Error getting geolocation data!');
-      showUserMsg("Geolocation is not supported", "error");
-    } else {
-      console.log('Getting geolocation data…');
-      navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
-    }
+  inputBox.value = "";
+}
 
+function sendGeo() {
+
+  const geoError = () => {
+    console.log('Error getting geolocation data!');
+    showUserMsg("Unable to get geolocation!", "error");
+  }
+
+  const geoSuccess = (position) => {
+    console.log('Getting coordinates:', position);
+    ws.send(position);
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    
+    showUserMsg(`
+    My latitude is ${latitude}° and
+    my longitude is ${longitude}°
+    `, "user");
+
+    showUserMsg(`https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`, `user`, true);
+  }
+
+  if (!navigator.geolocation) {
+    console.log('Error getting geolocation data!');
+    showUserMsg("Geolocation is not supported", "error");
+  } else {
+    console.log('Getting geolocation data…');
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+  }
+
+}
+
+function listenEnterKey() {
+  inputPanel.addEventListener("keyup", (event) => {
+   if (event.key === "Enter" && inputBox.value) {
+    console.log("Enter key pressed.");
+    sendMessage();
+   }
   });
 }
 
@@ -139,6 +162,8 @@ function showUserMsg(msg, type, geo) {
     userBubble.classList = "bubble bubble-input";
   } else if (type === "error") {
     userBubble.classList = "bubble bubble-output error";
+  } else if (type === "echo") {
+    userBubble.classList = "bubble bubble-output echo";
   } else {
     userBubble.classList = "bubble bubble-output";
   }
@@ -161,6 +186,8 @@ document.addEventListener("DOMContentLoaded", () => {
   wsConnect();
 
   initButtons();
+
+  listenEnterKey();
   
 });
 
